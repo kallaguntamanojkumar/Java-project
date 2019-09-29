751f4c03610302a90d5e3a161457da92e97a8eef
oney12 <manojkumarkallagunta12@gmail.com>
2019-09-30 02:10:38 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -25,6 +25,14 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
 
+uploadPkgUser=$1
+uploadPkgUserPassword=$2
+nexusRestAPIURL=$3
+nexusRepo=$4
+nexusMvnGroupID=$5
+artifactID=$6
+artifactVersion=$7
 
-curl --silent --output /dev/stderr --write-out "%{http_code}" -k -u ${uploadPkgUser}:${uploadPkgUserPassword} -X POST "${nexusRestAPIURL}/components?repository=${nexusRepo}" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "maven2.groupId=${nexusMvnGroupID}" -F "maven2.artifactId=${artifactID}" -F "maven2.version=${artifactVersion}" -F "maven2.generate-pom=false" -F "maven2.packaging=zip" -F "maven2.asset1=@${WORKSPACE}/target/${artifactID}.zip" -F "maven2.asset1.extension=zip" )
+
+curl --silent --output /dev/stderr --write-out "%{http_code}" -k -u ${uploadPkgUser}:${uploadPkgUserPassword} -X POST "${nexusRestAPIURL}/components?repository=${nexusRepo}" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "maven2.groupId=${nexusMvnGroupID}" -F "maven2.artifactId=${artifactID}" -F "maven2.version=${artifactVersion}" -F "maven2.generate-pom=false" -F "maven2.packaging=zip" -F "maven2.asset1=@${WORKSPACE}/target/${artifactID}.zip" -F "maven2.asset1.extension=zip"
                                                
